cb1c1130ef553403b09c7eaeeef81d30f2be813d
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-09-14 09:12:28 +0100

    finished hw

---
+++ library.js
@@ -0,0 +1,49 @@
+// const Library = function (books = []) {
+//   this.books = books;
+// };
+
+export default class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  bookCount() {
+    return this.books.length;
+  }
+
+  addBook(newBook) {
+    this.books.push(newBook);
+  }
+
+  addBooks(newBooks) {
+    newBooks.forEach(book => this.books.push(book));
+  }
+
+  printInventory() {
+    this.books.forEach((book) => {
+      const [author, title, genre] = book;
+      console.log(`${title} by ${author}`);
+    });
+  }
+}
+
+// Library.prototype.bookCount = function () {
+//   return this.books.length;
+// };
+
+
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
+
+// module.exports = Library;
